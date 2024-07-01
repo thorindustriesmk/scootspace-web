5e1fea4c2e5da35801a745e7d296fde2875b97d1
TaulantThor <taulant.avdili@thorindustriesmk.com>
2024-07-01 13:48:53 +0200

    changed footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,44 +8,37 @@ import ScootSpaceLogo from "../assets/logo/logo.svg";
 function Footer() {
   return (
     <>
-      <div className="flex flex-row xl:w-full w-[95%] pt-10  mb-10">
-        <div className="flex flex-col gap-6 justify items-start w-full xl:px-28 ">
-          <div className="flex flex-row justify-start items-start">
-            <ul className="flex flex-row justify-center items-start gap-7 text-center text-black text-2xl font-normal  leading-7">
-              <li>About Us</li>
-              <li>Our office</li>
-              <li>Our app</li>
-            </ul>
-          </div>
-          <div className="flex flex-row justify-end items-end ">
-            <img className="w-56" src={ScootSpaceLogo} alt="scootspace-logo" />
-          </div>
+      <div className="flex flex-row xl:w-full w-[100% py-10">
+        <div className="flex basis-2/12 gap-6 justify-center items-center w-full ">
+          <img
+            className="w-full px-8"
+            src={ScootSpaceLogo}
+            alt="scootspace-logo"
+          />
         </div>
 
-        <div className=" flex flex-col gap-6 justify-end items-end w-full xl:px-28">
-          <div className=" flex flex-row justify-end">
-            <ul className="flex flex-row gap-7 text-center text-black text-2xl font-normal  leading-7">
-              <li>
-                <img src={linkedin} alt="" />
-              </li>
-              <li>
-                <img src={facebook} alt="" />
-              </li>
-              <li>
-                <img src={instagram} alt="" />
-              </li>
-              <li>
-                <img src={twitter} alt="" />
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul className="flex flex-row justify-center items-center gap-7 text-center text-black text-2xl font-normal  leading-7">
-              <li>Terms</li>
-              <li>Privacy</li>
-              <li>Our app</li>
-            </ul>
-          </div>
+        <div className="flex basis-10/12 flex-col gap-6 items-end pr-8  w-full">
+          <ul className="flex flex-row gap-7 text-center text-black text-2xl font-normal  leading-7">
+            <li>
+              <img src={linkedin} alt="" />
+            </li>
+            <li>
+              <img src={facebook} alt="" />
+            </li>
+            <li>
+              <img src={instagram} alt="" />
+            </li>
+            <li>
+              <img src={twitter} alt="" />
+            </li>
+          </ul>
+
+          <ul className="flex flex-row justify-center items-center gap-7 text-center text-black text-2xl font-normal  leading-7">
+            <li>Our office</li>
+            <li>Terms</li>
+            <li>Privacy</li>
+            <li>Our app</li>
+          </ul>
         </div>
       </div>
     </>
